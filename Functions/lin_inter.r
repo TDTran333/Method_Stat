36375959ad4d3836a70d941eb51fe5b6dc799f38
GroulxFA <felix-antoine.groulx@hec.ca>
2019-11-21 13:20:59 -0500

    +Vérification et polissage du code

--- Functions/lin_inter.r
+++ Functions/lin_inter.r
@@ -1,21 +1,19 @@
-
 lin_inter <- function(day) {
   
-  
-  for (i in 1:6){
-  if (day > as.numeric(attributes(rf[i]))){
-     tmp1 <- i
-     tmp2 <- i + 1
+  for (i in 1:nrow(rates)) {
+    if (day > rates[i,1]) {
+       lb <- i
+       ub <- i + 1
+      }
   }
-  }
   
-  num   <- day - as.numeric(attributes(rf[tmp1]))
-  denum <- as.numeric(attributes(rf[tmp2])) - as.numeric(attributes(rf[tmp1]))
+  num   <- day - rates[lb,1]
+  denum <- rates[ub,1] - rates[lb,1]
   
-  w1 <- num / denum
-  w2 <- 1 - w1
+  w_1 <- num / denum
+  w_2 <- 1 - w_1
     
-  r <- w1 * rf[tmp2] + w2 *rf[tmp1]
+  r <- w_1 * rates[ub,2] + w_2 * rates[lb,2]
   
   return(r)
 }
