c949d1ac7f293d11e231275da0b8d2fb22e4e582
GroulxFA <felix-antoine.groulx@hec.ca>
2019-11-22 10:39:28 -0500

    +Commentaires

--- Functions/lin_inter.r
+++ Functions/lin_inter.r
@@ -1,5 +1,10 @@
 lin_inter <- function(day) {
+  # This function takes as input a maturity in year (360-day basis)
+  # and outputs the interpolated risk-free rate using a term structure
+  # specified in the assignment.
   
+  # Loop through the term structure and identify the largest smaller
+  # maturity (lb) and the lowest larger maturity (ub)
   for (i in 1:nrow(rates)) {
     if (day > rates[i,1]) {
        lb <- i
@@ -7,13 +12,16 @@ lin_inter <- function(day) {
       }
   }
   
+  # Compute the numerator and the denominator used to compute 'w_1'
   num   <- day - rates[lb,1]
   denum <- rates[ub,1] - rates[lb,1]
   
   w_1 <- num / denum
   w_2 <- 1 - w_1
-    
+  
+  # Compute the interpolated risk-free rate
   r <- w_1 * rates[ub,2] + w_2 * rates[lb,2]
   
+  # Output the interpolated risk-free rate
   return(r)
 }
--- Functions/price_call.r
+++ Functions/price_call.r
@@ -1,13 +1,23 @@
-Price_call <- function(S, K, r, vol, N) {
+Price_call <- function(S, K, r, vol, M) {
+  # This function takes as input the underlying asset price (S), the
+  # option strike price (K), the risk-free rate (r), the volatility
+  # (vol), and the option time to expiry (M) in year (360-day basis).
   
-  d1 <- (log(S/K) + (r + vol^2 / 2) * N) / (vol * N^0.5)
-  d2 <- d1 - vol * N^0.5
+  # The function outputs the price of a call option with those five
+  # parameters using the Black-Scholes (1973) model.
   
-  price <- S * pnorm(d1) - K * exp(r * N) * pnorm(d2)  
+  # Compute the quantities 'd_1' and 'd_2'
+  d_1 <- (log(S / K) + (r + vol^2 / 2) * N) / (vol * N^0.5)
+  d_2 <- d_1 - vol * N^0.5
   
-  if (price < 0){
+  # Compute the call option price
+  price <- S * pnorm(d_1) - K * exp(r * N) * pnorm(d_2)
+  
+  # Make sure the call options price is larger or equal to zero
+  if (price < 0) {
       price <- 0
   }
   
+  # Output the call option price
   return(price)
 }
