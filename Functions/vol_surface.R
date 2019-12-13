b9263212e0a474342a01f0a70b64224a38cd8947
TDTran333 <56975352+TDTran333@users.noreply.github.com>
2019-12-13 11:49:49 -0500

    Updates

--- Functions/vol_surface.R
+++ Functions/vol_surface.R
@@ -1,7 +1,7 @@
 vol_surface <- function(S, K, M, a1, a2, a3, a4) {
   # This function takes as input the underlying asset price (S), the
   # option strike price (K), the option time to expiry (M) in year
-  # (360-day basis), and four other parameters named a1, a2, a3, a4.
+  # (250-day basis), and four other parameters named a1, a2, a3, a4.
   
   # The function outputs the implied volatility from the parametric
   # volatility surface.
